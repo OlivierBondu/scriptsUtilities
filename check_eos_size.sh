2bef4b15fdf3b053e37027f29b83b6f4cf3513c2
Olivier <olivier.bondu@gmail.com>
2013-06-28 15:21:48 +0200

    Code cleaning

--- check_eos_size.sh
+++ check_eos_size.sh
@@ -1,40 +1,60 @@
 #!/bin/bash
+# Script to check size of eos directory
+# Written by O. Bondu (June 2013)
 
+#####
+echo "WARNING: script has to be SOURCE (source script.sh) for the eos commands to be recognized"
+totalBytes="0"
 
+#####
 function letsdothis {
-if [[ -z ${1} ]]
-then
-	return 1
-fi
-
-folder=${1}
-oldfolder=${2}
-echo "checking folder ${folder}"
-
-#echo "summing files in the directory"
-# summing files in the directory
-for file in `eos ls ${folder}`
-do
-#	echo ${file}
-	eos ls -l ${folder} | grep -x ".*${file}\>" | awk '{print $1}' | grep d &> /dev/null && continue
-	eos ls -l ${folder}/${file}
-done | awk '{SUM+=$5} END{print SUM}' | awk '{ sum=$1 ; hum[1024**3]="Gb";hum[1024**2]="Mb";hum[1024]="Kb"; for (x=1024**3; x>=1024; x/=1024){ if (sum>=x) { printf "%.2f %s\n",sum/x,hum[x];break } }}'
-
-#echo "listing additional directories"
-# listing additional directories
-for file in `eos ls ${folder}`
-do
-#		echo ${folder}/${file}
-#		eos ls -l ${folder}
-#	  eos ls -l ${folder} | grep -x ".*${file}\>" | awk '{print $1}' | grep d &> /dev/null && echo -e "\tNew folder= ${file}" && letsdothis ${folder}/${file} ${folder}
-	  eos ls -l ${folder} | grep -x ".*${file}\>" | awk '{print $1}' | grep d &> /dev/null && letsdothis ${folder}/${file} ${folder}
-done
-
-folder=${oldfolder}
-#echo "exiting function: folder= ${folder}"
-return
+	if [[ -z ${1} ]]
+	then
+		return 1
+	fi
+	
+	folder=${1}
+	oldfolder=${2}
+	echo "checking folder ${folder}"
+	
+	#####
+	#echo "summing files in the directory"
+	totalfiles=$(
+	for file in `eos ls ${folder}`
+	do
+	#	echo ${file}
+		eos ls -l ${folder} | grep -x ".*${file}\>" | awk '{print $1}' | grep d &> /dev/null && continue
+		eos ls -l ${folder}/${file}
+	done | awk '{SUM+=$5} END{print SUM}'
+	)
+	if [[ ${totalfiles} = "" ]]
+	then
+		totalfiles="0"
+	fi
+	
+	totalfilesHR=`echo "${totalfiles}" | awk '{ sum=$1 ; hum[1024**5]="Pb";hum[1024**4]="Tb";hum[1024**3]="Gb";hum[1024**2]="Mb";hum[1024]="Kb"; for (x=1024**5; x>=1024; x/=1024){ if (sum>=x) { printf "%.2f %s\n",sum/x,hum[x];break } }}'`
+	echo -e "totalfiles=\t${totalfilesHR}\t(${totalfiles} bytes)"
+	
+	totalBytes=$(( ${totalfiles} + ${totalBytes} ))
+	
+	#####
+	#echo "listing additional directories"
+	total="0"
+	for file in `eos ls ${folder}`
+	do
+	#		echo ${folder}/${file}
+		  eos ls -l ${folder} | grep -x ".*${file}\>" | awk '{print $1}' | grep d &> /dev/null && letsdothis ${folder}/${file} ${folder}
+	done
+	
+	folder=${oldfolder}
+	#echo "exiting function: folder= ${folder}"
+	return ${total}
 }
 
 letsdothis ${1} ${1}
-#letsdothis /eos/cms/store/group/phys_higgs/Resonant_HH/reduced/radion_reduction_v03 /eos/cms/store/group/phys_higgs/Resonant_HH/reduced/radion_reduction_v03
+
+##### 
+totalBytesHR=`echo "${totalBytes}" | awk '{ sum=$1 ; hum[1024**5]="Pb";hum[1024**4]="Tb";hum[1024**3]="Gb";hum[1024**2]="Mb";hum[1024]="Kb"; for (x=1024**5; x>=1024; x/=1024){ if (sum>=x) { printf "%.2f %s\n",sum/x,hum[x];break } }}'`
+echo ""
+echo -e "GRAND TOTAL =\t${totalBytesHR}\t(${totalBytes} bytes)\tfor folder ${1}"
 
